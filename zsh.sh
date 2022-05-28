8a7e84452a7cbfff4cefb3717cdd96852fb88b43
White-devil <66563771+harshmavani24@users.noreply.github.com>
2022-05-28 17:46:03 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,15 +1,23 @@
-sudo apt install zsh
+sudo apt install zsh figlet lolcat -y
 sleep 4
 sudo apt install curl wget git -y
 sleep 4
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+cd lolcat-master/bin
+gem install lolcat
+sleep 2
+cd /root
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sleep 5
-source .zshrc
-sleep 5
+sleep 5 
+cd ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 sleep 2
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 sleep 2
-rm -rf .zshrc
+rm -rf ~/.zshrc
+cd /root
 git clone https://github.com/harshmavani24/zsh.git
-source .zshrc
+cd zsh
+cp .zshrc ~/.zshrc
+source ~/.zshrc
